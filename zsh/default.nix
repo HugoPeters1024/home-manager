d310cc278280ce9c44cbafff8b0854ac78166050
Hugo Peters <hpeters1024@gmail.com>
2025-09-16 11:12:47 +0200

    fix some relative paths

--- zsh/default.nix
+++ zsh/default.nix
@@ -1,14 +1,13 @@
 {
   pkgs,
-  lib,
-  username,
+  config,
   ...
 }:
 {
   programs.zsh = {
     enable = true;
     autocd = true;
-    dotDir = ".config/zsh";
+    dotDir = "${config.home.homeDirectory}/.config/zsh";
     enableCompletion = true;
     history.size = 100000;
     oh-my-zsh = {
@@ -23,17 +22,13 @@
     };
     envExtra = ''
       # use whatever installed by rustup
-      export PATH="/home/hugo/.cargo/bin:$PATH"
-      export PATH="/Users/hpeters/.cargo/bin:$PATH"
-      export SHELL=${pkgs.zsh}/bin/zsh
+      export PATH="${config.home.homeDirectory}/.cargo/bin:$PATH"
 
       # local bin is fine too
-      export PATH="/Users/hpeters/.local/bin:$PATH"
+      export PATH="${config.home.homeDirectory}/.local/bin:$PATH"
 
       # use direnv
       eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
-
-      alias with-cachix-key="vaultenv --secrets-file  <(echo \"cachix#signing-key\" ) -- "
     '';
 
     initContent = ''
