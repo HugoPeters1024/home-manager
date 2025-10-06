4e88d774e8a35ff75b547f1a7fa0805c96fd4dfc
Hugo Peters <hpeters1024@gmail.com>
2025-10-06 13:29:48 +0200

    move branch lualine to the right

--- nvim/default.nix
+++ nvim/default.nix
@@ -177,9 +177,9 @@ in
         },
         sections = {
           lualine_a = {'mode'},
-          lualine_b = {'branch'},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_b = {'filename'},
+          lualine_c = {},
+          lualine_x = {'branch', 'encoding', 'fileformat', 'filetype'},
           lualine_y = {},
           lualine_z = {}
         },
